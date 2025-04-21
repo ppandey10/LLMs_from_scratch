1857ba79bacd6a5e3a524ac7bfa405d146758d4f
Prasoon Pandey <pandey.prasoon2000@gmail.com>
2025-04-21 20:38:12 +0200

    restructuring

--- nakliGPT/src/decoder.py
+++ nakliGPT/src/decoder.py
@@ -1,17 +1,66 @@
 """
-Implementation of decoder components for the nakliGPT such as:
-
+Combining decoder components to build the nakliTransformerDecoder
 - Multi-Head Attention
-- Feed Forward Network
 - Layer Normalization
-- GELU Activation=
+- Residual Connection
+- Feed Forward Network
 """
 
-import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+#======================#
+
+class nakliTransformerDecoder(nn.Module):
+    """
+    Implementation of nakliTransformerDecoder
+    """
+    def __init__(
+        self,
+        embd_dim: int, 
+        num_heads: int,
+        context_length: int,
+        dropout: float,
+        qkv_bias: bool = False,
+    ): 
+        super().__init__()
+        self.attention = nakliMultiHeadAttention(
+            in_dim=embd_dim,
+            out_dim=embd_dim,
+            context_length=context_length,
+            num_heads=num_heads,
+            qkv_bias=qkv_bias,
+        )
+        self.layer_norm_1 = nn.LayerNorm(embd_dim)
+        self.layer_norm_2 = nn.LayerNorm(embd_dim)
+        self.feed_forward = nakliFeedForward(embd_dim)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        temp_x = x
+        #==================#
+        # Multi-Head Attention
+        #==================#
+        x = self.layer_norm_1(temp_x)
+        x = self.attention(x)
+        x = self.dropout(x)
+        x = temp_x + x # residual
+
+        #==================#
+        # Feed Forward
+        #==================#
+        temp_x = x # change temporary x
+        x = self.layer_norm_2(temp_x)
+        x = self.feed_forward(x)
+        x = self.dropout(x)
+        x = temp_x + x
+        
+        return x
+
+#======================#
+# Multi-Head Attention
+#======================#
 class nakliMultiHeadAttention(nn.Module):
     """
     Implementation of efficient Multi-Head Attention
@@ -86,7 +135,9 @@ class nakliMultiHeadAttention(nn.Module):
 
         return output
 
-
+#======================#
+# Feed Forward
+#======================#
 class nakliFeedForward(nn.Module):
     """
     Implementation of feed forward network
@@ -110,22 +161,3 @@ class nakliFeedForward(nn.Module):
 #     output = mha.forward(x)
 #     print(output.shape)
     
-        
-        
-
-
-
-
-
-
-
-
-
-
-        
-
-
-        
-
-
-
